c07ab8815fd380261162afe1912b85f96ba4b251
Nasser <abdounasser202@gmail.com>
2020-10-30 19:00:14 +0100

    2- integrates owl into odoo

--- modules/hello_owl/static/src/js/hello.js
+++ modules/hello_owl/static/src/js/hello.js
@@ -1,1 +1,13 @@
-console.log('hellooooOOOOOooOOO!!!!!')
+odoo.define('hello_owl.hello', function(require) {
+
+    'use strict';
+
+    const { Component } = owl;
+
+    class App extends Component {
+      template = 'App'
+    }
+
+    return App;
+
+})
